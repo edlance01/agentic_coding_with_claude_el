07eebb246651c20c44cd5291cede9e3c66dde8f8
edlance1 <edlance@ntiertraining.com>
2026-07-07 12:54:50 -0400

    added play_sound Stop hook

---
+++ hookhub/hooks-notification/play_sound.py
@@ -0,0 +1,11 @@
+import subprocess
+from pathlib import Path
+
+
+def main():
+    sound = Path(__file__).parent / "ulala.wav"
+    subprocess.run(["afplay", str(sound)], check=True)
+
+
+if __name__ == "__main__":
+    main()
